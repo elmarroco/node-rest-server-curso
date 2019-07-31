af178a11b69df2a92cf44392dca566d09a19e922
Jose Masri <josemasri222@gmail.com>
2019-07-31 11:40:47 -0500

    google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -32,3 +32,10 @@ process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
 const urlDB = process.env.MONGO_URI || "mongodb://localhost:21018/cafe";
 
 process.env.URLDB = urlDB;
+
+// ================================
+// Google client id
+//=================================
+process.env.CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "215062113832-jjpvi8gn66td6gmgivlh024misbk232j.apps.googleusercontent.com";
