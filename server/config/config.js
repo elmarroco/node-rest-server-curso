7af7fda23b33ec007c2ba73c70d273a2611d869e
Jose Masri <josemasri222@gmail.com>
2019-07-30 20:23:22 -0500

    Varible de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,17 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // ================================
 // Base de datos
 //=================================
-let urlDB;
-const user = "cafeUser";
-const password = "Jose123456";
-
-if ((process.env.NODE_ENV = "dev")) {
-  urlDB = "mongodb://localhost:21018/cafe";
-} else {
-  urlDB = `mongodb+srv://${user}:${password}@cluster0-tp4og.mongodb.net/test?retryWrites=true&w=majority`;
-}
-
-urlDB = `mongodb+srv://${user}:${password}@cluster0-tp4og.mongodb.net/test?retryWrites=true&w=majority`;
+const urlDB = process.env.MONGO_URI || "mongodb://localhost:21018/cafe";
 
 
 process.env.URLDB = urlDB;
